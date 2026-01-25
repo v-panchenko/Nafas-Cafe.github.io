f5b91deece31bde266453c02738661264e3dda65
Vladislav <vladislavpanchenko2008@gmail.com>
2026-01-26 01:06:53 +0400

    Delete js directory

--- js/menu-script.js
+++
@@ -1,8 +0,0 @@
-const backToTop = document.getElementById("Home-navigation")
-window.addEventListener("scroll",() => {
-  if (window.scrollY > 700) {
-    backToTop.classList.add("show");
-  } else {
-    backToTop.classList.remove("show");
-  }
-});
